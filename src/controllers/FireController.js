b9792c20a6116b9930930f4ef2921c5b1dc10c09
Danilo <mattosgdanilo@gmail.com>
2019-10-20 15:46:08 -0300

    Atualiza master

--- src/controllers/FireController.js
+++ src/controllers/FireController.js
@@ -10,9 +10,6 @@ class FireController {
   }
 
   async show(req, res) {
-    req.headers = {
-      Authorization: "Bearier 5E172F90-F2F1-11E9-83D8-84F3207B60E0"
-    };
     try {
       // const fire = await axios.get(
       //   "https://nrt4.modaps.eosdis.nasa.gov/api/v2/content/archives/FIRMS/viirs/South_America/VIIRS_I_South_America_VNP14IMGTDL_NRT_2019233.txt",
@@ -24,25 +21,25 @@ class FireController {
       // );
       const csvFilePath =
         "/Users/Danilo/dracarys-backend/csv/VIIRS_I_South_America_VNP14IMGTDL_NRT_2019274.txt";
-      // csv()
-      //   .fromFile(csvFilePath)
-      //   .then(jsonObj => {
-      //     console.log(jsonObj);
-      //     /**
-      //      * [
-      //      * 	{a:"1", b:"2", c:"3"},
-      //      * 	{a:"4", b:"5". c:"6"}
-      //      * ]
-      //      */
-      //   });
+
+      csv()
+        .fromFile(csvFilePath)
+        .then(jsonObj => {
+          console.log(jsonObj);
+          /**
+           * [
+           * 	{a:"1", b:"2", c:"3"},
+           * 	{a:"4", b:"5". c:"6"}
+           * ]
+           */
+        });
 
       // Async / await usage
       const jsonArray = await csv().fromFile(csvFilePath);
+      return res.send(jsonArray);
     } catch (err) {
       console.log(err);
     }
-
-    return res.send({ jsonArray });
   }
 }
 
